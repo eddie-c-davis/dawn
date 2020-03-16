ae32acd263054897e0f790cd6ed43b4bfd205af9
Eddie Davis <eddie-c-davis@u.boisestate.edu>
2020-03-16 21:03:43 +0000

    Update compile scripts

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ $1.cpp -I/home/eddied/Work/dawn/dawn/src -I/home/eddied/Work/dawn/gtclang -I/home/eddied/Work/dawn/build/_deps/gridtools-src/include -I/home/eddied/Work/dawn/build/_deps/googletest-src/googletest/include -g -O3 -fopenmp -o $1
+g++ $1.cpp -I/home/eddied/dawn/dawn/src -I/home/eddied/dawn/gtclang -I/home/eddied/dawn/gtclang/src -I/home/eddied/dawn/build/_deps/gridtools-src/include -I/home/eddied/dawn/build/_deps/googletest-src/googletest/include -std=c++11 -fopenmp -g -O3 -o $1
--- scripts/cuda_compile.sh
+++ scripts/cuda_compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DAWN_PATH=/home/eddied/dawn
-nvcc $1.cu -DGRIDTOOLS_DAWN_CUDA -DOPTBACKEND=cuda -arch=sm_60 -g -O3 -std=c++11 -I${DAWN_PATH}/dawn/src -I/${DAWN_PATH}/gtclang -I${DAWN_PATH}/gtclang/src -I${DAWN_PATH}/build/_deps/gridtools-src/include -o ${1}_cu
+nvcc $1.cu -DGRIDTOOLS_DAWN_CUDA -DOPTBACKEND=cuda -x cu -arch=sm_60 -g -O3 -std=c++11 -I${DAWN_PATH}/dawn/src -I/${DAWN_PATH}/gtclang -I${DAWN_PATH}/gtclang/src -I${DAWN_PATH}/build/_deps/gridtools-src/include -o ${1}_cu
